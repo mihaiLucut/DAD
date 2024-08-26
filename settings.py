1a0db6eaf495a6831401c2daac69344acba651ff
mihaiLucut <mihai.lucut@gmail.com>
2024-08-26 12:28:41 +0200

    settings and config

--- settings.py
+++ settings.py
@@ -27,7 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'your_app_name',  # Replace with your actual app name
+    'dad',
 ]
 
 MIDDLEWARE = [
